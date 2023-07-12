9366ef9fa2a599d777d1a72bef3ad437c7c3bc29
Jesse Lopez <lopezjesse@microsoft.com>
2023-07-12 05:26:36 +0000

    Updates for CUDA 12

--- opt-175b/install-opt175.sh
+++ opt-175b/install-opt175.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Installs environment to run OPT-175B training benchmarks.
-
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -8,29 +7,37 @@ set -o pipefail
 # These env vars are used eventually
 source nccl-env-var.sh
 
-INSTALL_DIR=${INSTALL_DIR-${HOME}}
+INSTALL_DIR=${INSTALL_DIR-$HOME}
 SRC_DIR=`readlink -f .`
+CONDA_DIR=/mnt/miniconda
 
 printf "Downloading and installing software in ${INSTALL_DIR}"
 pushd $INSTALL_DIR
 
 printf "Installing conda"
 curl -fsO https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
-$HOME/miniconda/bin/conda init
-source $HOME/.bashrc
+bash Miniconda3-latest-Linux-x86_64.sh -b -p $CONDA_DIR
+
+# Setup conda for shell use
+$CONDA_DIR/bin/conda init `basename $SHELL`
+# Cannot directly source .bashrc on Ubuntu from script, so just directly source the conda script
+source $CONDA_DIR/etc/profile.d/conda.sh
+
 conda create -y -c conda-forge --name fairseq python=3.9
 conda activate fairseq
 
 printf "Installing Python requirements"
-pip install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
+#CUDA 11.3/11.4
+#python -m pip install -r $SRC_DIR/requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
+#CUDA 12
+python -m pip install -r $SRC_DIR/requirements.txt --pre -f https://download.pytorch.org/whl/nightly/cu121/torch_nightly.html
 
 printf "Installing Apex"
 git clone https://github.com/NVIDIA/apex
 pushd apex
 # Avoid CUDA extension + Pytorch complaint.  This is fine for CUDA 11.3 vs. 11.4
 sed -i "s/check_cuda_torch_binary_vs_bare_metal(CUDA_HOME)//g" setup.py
-python -m pip install -v --no-cache-dir --global-option="--cpp_ext" \
+python -m pip install -v --no-build-isolation --no-cache-dir --global-option="--cpp_ext" \
     --global-option="--cuda_ext" \
     --global-option="--deprecated_fused_adam" \
     --global-option="--xentropy" \
@@ -47,7 +54,7 @@ printf "Installing Megatron fork"
 git clone https://github.com/ngoyal2707/Megatron-LM.git
 pushd Megatron-LM
 git checkout fairseq_v3
-pip install -e
+pip install -e .
 popd
 
 printf "Installing Metaseq"
--- opt-175b/nccl-env-var.sh
+++ opt-175b/nccl-env-var.sh
@@ -5,3 +5,8 @@ export NCCL_TOPO_FILE=/opt/microsoft/ndv4-topo.xml
 export OMPI_MCA_coll_hcoll_enable=0
 export UCX_IB_PCI_RELAXED_ORDERING=on
 export UCX_NET_DEVICES=eth0
+# Some skus do not have sufficient disk space to build conda env for OPT.
+# To avoid problems, specify the cache on the ephemeral disk
+export PIP_CACHE_DIR=/mnt/.cache/pip
+export INSTALL_DIR=/mnt/opt
+export TMPDIR=/mnt/tmp
